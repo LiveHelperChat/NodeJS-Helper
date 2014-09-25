d225666c3026ee63c62f737cadd774a540ee2bf1
Remdex <remdex@gmail.com>
2014-09-25 17:40:41 +0200

    Option to do not specify port

--- nodejshelper/design/nodejshelpertheme/tpl/pagelayouts/parts/nodejshelper_config.tpl.php
+++ nodejshelper/design/nodejshelpertheme/tpl/pagelayouts/parts/nodejshelper_config.tpl.php
@@ -2,7 +2,7 @@
 	$nodeJsHelperSettings = array (
 		'prefix' => 'http://',
 		'host' => 'localhost',
-		'port' => '31129',
+		'port' => ':31129',
 		'secure' => false,
 		'use_cdn' => true,	// Should we use google provided socket.io.js library?, if you are using older version set it to false
 		'use_publish_notifications' => false,
--- nodejshelper/design/nodejshelpertheme/tpl/pagelayouts/parts/page_head_js_user_extension_multiinclude.tpl.php
+++ nodejshelper/design/nodejshelpertheme/tpl/pagelayouts/parts/page_head_js_user_extension_multiinclude.tpl.php
@@ -1,7 +1,7 @@
 <?php include(erLhcoreClassDesign::designtpl('pagelayouts/parts/nodejshelper_config.tpl.php'));?>
 <?php if ( (isset($Result['is_sync_required']) && $Result['is_sync_required'] === true) || erLhcoreClassSystem::instance()->SiteAccess == 'site_admin' ) : ?>
 <script>
-var nodejshelperHostConnect = '<?php echo $nodeJsHelperSettings['host']?>:<?php echo $nodeJsHelperSettings['port']?>';
+var nodejshelperHostConnect = '<?php echo $nodeJsHelperSettings['host']?><?php echo $nodeJsHelperSettings['port']?>';
 var nodejshelperConfig = {use_publish_notifications:<?php echo $nodeJsHelperSettings['use_publish_notifications'] == true ? 'true' : 'false'?>,instance_id:'<?php echo $nodeJsHelperSettings['instance_id']?>',secure:<?php echo $nodeJsHelperSettings['secure'] == true ? 'true' : 'false' ?>,'typer':'','sync':<?php echo (isset($Result['chat']) && $Result['chat']->status == erLhcoreClassModelChat::STATUS_PENDING_CHAT) ? 'true' : 'false'?>,'synctimeout':5};
 <?php if (erLhcoreClassSystem::instance()->SiteAccess == 'site_admin' && erLhcoreClassUser::instance()->isLogged()) : 
 $currentUser = erLhcoreClassUser::instance();
@@ -12,7 +12,7 @@ nodejshelperConfig.typer = '<?php echo htmlspecialchars($userData->name_support,
 <?php if ($nodeJsHelperSettings['use_cdn'] == true) : ?>
 <script src="https://cdn.socket.io/socket.io-1.1.0.js"></script>
 <?php else : ?>
-<script src="<?php echo $nodeJsHelperSettings['prefix'],$nodeJsHelperSettings['host']?>:<?php echo $nodeJsHelperSettings['port']?>/socket.io/socket.io.js"></script>
+<script src="<?php echo $nodeJsHelperSettings['prefix'],$nodeJsHelperSettings['host']?><?php echo $nodeJsHelperSettings['port']?>/socket.io/socket.io.js"></script>
 <?php endif;?>
 <script type="text/javascript" language="javascript" src="<?php echo erLhcoreClassDesign::designJS('js/customjs.js');?>"></script>
 <?php endif; ?>
