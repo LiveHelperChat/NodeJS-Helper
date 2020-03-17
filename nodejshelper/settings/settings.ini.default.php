d1c29d6925a6c3ea155a058cce06e6730e6b74b8
Remigijus Kiminas <remdex@gmail.com>
2020-03-17 10:12:48 -0400

    Adjust tutorial to new version

--- nodejshelper/settings/settings.ini.default.php
+++ nodejshelper/settings/settings.ini.default.php
@@ -5,7 +5,7 @@ return array(
     'connect_db_id' => 0,
     'automated_hosting' => false,
     'public_settings' => array(
-        'hostname' => '//' . $_SERVER['HTTP_HOST'],
+        'hostname' => (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null),
         'path' => '/socketcluster/',
         'port' => null, //some custom port
         'secure' => null, // true || false
