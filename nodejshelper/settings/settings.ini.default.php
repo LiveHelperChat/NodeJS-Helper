5d512eb81ccdf8ae08a03e1689b2f0162b4a1cec
Remigijus Kiminas <remdex@gmail.com>
2020-10-01 03:33:57 -0400

    omit port from hostname

--- nodejshelper/settings/settings.ini.default.php
+++ nodejshelper/settings/settings.ini.default.php
@@ -5,7 +5,7 @@ return array(
     'connect_db_id' => 0,
     'automated_hosting' => false,
     'public_settings' => array(
-        'hostname' => (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : null),
+        'hostname' => (isset($_SERVER['HTTP_HOST']) ? explode(':',$_SERVER['HTTP_HOST'])[0] : null),
         'path' => '/socketcluster/',
         'port' => null, //some custom port
         'secure' => null, // true || false
