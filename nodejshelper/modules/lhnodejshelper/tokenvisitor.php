c44e3ec19246c260706d8474f3102a460ec583d3
Remigijus Kiminas <remdex@gmail.com>
2021-01-28 12:22:29 -0500

    Make sure chat is not closed

--- nodejshelper/modules/lhnodejshelper/tokenvisitor.php
+++ nodejshelper/modules/lhnodejshelper/tokenvisitor.php
@@ -7,7 +7,15 @@ $chatId = $Params['user_parameters']['chat_id'];
 $date = time();
 
 if (is_numeric($chatId)) {
-    echo json_encode(sha1($date . 'Visitor' . erConfigClassLhConfig::getInstance()->getSetting('site','secrethash') .'_' . $chatId) . '.' . $date);
+
+    $chat = erLhcoreClassModelChat::fetch($chatId);
+
+    if ($chat instanceof erLhcoreClassModelChat && $chat->hash == $Params['user_parameters']['hash'] && $chat->status !== erLhcoreClassModelChat::STATUS_CLOSED_CHAT) {
+        echo json_encode(sha1($date . 'Visitor' . erConfigClassLhConfig::getInstance()->getSetting('site','secrethash') .'_' . $chatId) . '.' . $date);
+    } else {
+        echo json_encode("invalid");
+    }
+
 } else {
     echo json_encode(sha1($date . 'Visitor' . erConfigClassLhConfig::getInstance()->getSetting('site','secrethash')) . '.' . $date);
 }
